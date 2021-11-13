1ae42af21cd3864ceca6d46c9bc6848bfcf3eea4
Vijaysrinivas Rajagopal <vijaythinks@gmail.com>
2021-11-13 16:12:48 -0500

    exp structure set

--- exp.py
+++ exp.py
@@ -2,6 +2,7 @@ import numpy as np
 import models
 import reducer
 import argparse
+import os
 
 def parseArgs():
     args = argparse.ArgumentParser()
@@ -9,5 +10,20 @@ def parseArgs():
     args.add_argument('--data', '-d', default='./data/ashrae')
     return args.parse_args()
 
+def expRun(exp_name:str, *exp_kwargs):
+    if exp_name == 'proto1':
+        # run prototype stuff here:
+        pass
+    else:
+        raise Exception("Unrecognized experiment name!")
+
+# All the experiments we want to do with different classifiers and stuff:
+
+# Due 11/18/2021:
+def prototype1(data_path):
+    # run data processing for training, testing etc. here:
+    train_file = os.path.join(data_path, 'train.csv')
+
 if __name__ == '__main__':
     args = parseArgs()
+    expRun(args.exp, args.data)
