327e32547231109c1e6a278ce003e5c93fcd4284
Vijaysrinivas Rajagopal <vijaythinks@gmail.com>
2021-11-13 16:38:46 -0500

    converge2

--- exp.py
+++ exp.py
@@ -3,10 +3,7 @@ import models
 import reducer
 import argparse
 import os
-<<<<<<< HEAD
 from data import data
-=======
->>>>>>> 9e7dba54434fa3908fd3a2d549364653650fa2c3
 
 def parseArgs():
     args = argparse.ArgumentParser()
@@ -17,27 +14,15 @@ def parseArgs():
 def expRun(exp_name:str, *exp_kwargs):
     if exp_name == 'proto1':
         # run prototype stuff here:
-<<<<<<< HEAD
         prototype1(*exp_kwargs)
     else:
         raise Exception("Unrecognized experiment name!")
 
-=======
-        pass
-    else:
-        raise Exception("Unrecognized experiment name!")
-
-# All the experiments we want to do with different classifiers and stuff:
-
->>>>>>> 9e7dba54434fa3908fd3a2d549364653650fa2c3
 # Due 11/18/2021:
 def prototype1(data_path):
     # run data processing for training, testing etc. here:
     train_file = os.path.join(data_path, 'train.csv')
-<<<<<<< HEAD
     data.preprocessBuildingData(train_file)
-=======
->>>>>>> 9e7dba54434fa3908fd3a2d549364653650fa2c3
 
 if __name__ == '__main__':
     args = parseArgs()
